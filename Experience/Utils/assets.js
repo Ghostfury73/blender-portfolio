ee2935066aba8333f508d5a257ce16309c6e6ade
unknown <amritrout73@gmail.com>
2023-10-30 00:46:30 +0530

    PORTFOLIO V3

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    name: "room",
+    type: "glbModel",
+    path: "/models/room_v3.glb",
+  },
+  {
+      name: "screen",
+      type: "videoTexture",
+      path: "/textures/k2.mp4",
+    },
+];
